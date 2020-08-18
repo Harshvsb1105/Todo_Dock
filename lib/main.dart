0189b10ce0c472f9c589b8c3a9d3e069307b5a18
Harshvardhan <harshvsb1105@gmail.com>
2020-08-18 16:02:15 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,109 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+import 'bloc/settings.dart';
+import 'bloc/todo.dart';
+import 'router.dart';
+import 'style.dart';
+import 'widgets/neo_app_style.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+
+  //only portrait
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  //fonts license
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
+/*
+  runApp(new SplashScreen());
+  var foo = await init();
+  runApp(new FullApp(foo: foo));
+*/
+
+  runApp(
+    EasyLocalization(
+        supportedLocales: [
+          const Locale('en'),
+          const Locale('fr'),
+          const Locale('ru')
+        ],
+        path: 'assets/locales',
+        fallbackLocale: const Locale('en'),
+        useOnlyLangCode: true,
+        preloaderColor: prefs.getBool('darkMode') == true
+            ? Style.bgColorDark
+            : Style.bgColor,
+        child: MultiProvider(providers: [
+          ChangeNotifierProvider(create: (_) => Todo(), lazy: false),
+          ChangeNotifierProvider(create: (_) => Settings(prefs), lazy: false)
+        ], child: const MyApp())),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return NeumorphicAppStyle(
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        debugShowCheckedModeBanner: false,
+        locale: context.locale,
+        title: 'title'.tr(),
+        initialRoute: '/',
+        onGenerateRoute: geneateRoute,
+        themeMode: context.watch<Settings>().themeMode,
+        theme: NeumorphicThemeData(
+            defaultTextColor: Style.textColor,
+            baseColor: Style.bgColor,
+            accentColor: Style.primaryColor,
+            variantColor: Style.primaryColor,
+            intensity: 0.6,
+            lightSource: LightSource.topRight,
+            depth: 3,
+            appBarTheme: NeumorphicAppBarThemeData(
+                buttonPadding: const EdgeInsets.all(14.0),
+                buttonStyle: const NeumorphicStyle(
+                    boxShape: NeumorphicBoxShape.circle()),
+                iconTheme: IconThemeData(
+                  color: Style.textColor,
+                ),
+                icons: NeumorphicAppBarIcons(
+                    backIcon: Icon(FontAwesomeIcons.chevronLeft),
+                    menuIcon: Icon(FontAwesomeIcons.bars)))),
+        darkTheme: NeumorphicThemeData(
+            defaultTextColor: Style.textColorDark,
+            baseColor: Style.bgColorDark,
+            accentColor: Style.primaryColor,
+            variantColor: Style.primaryColor,
+            intensity: 0.6,
+            lightSource: LightSource.topRight,
+            shadowDarkColor: Colors.black,
+            shadowLightColor: Colors.grey[500],
+            depth: 3,
+            appBarTheme: NeumorphicAppBarThemeData(
+                buttonPadding: const EdgeInsets.all(14.0),
+                buttonStyle: const NeumorphicStyle(
+                    boxShape: NeumorphicBoxShape.circle()),
+                iconTheme: IconThemeData(
+                  color: Style.textColorDark,
+                ),
+                icons: NeumorphicAppBarIcons(
+                    backIcon: Icon(FontAwesomeIcons.chevronLeft),
+                    menuIcon: Icon(FontAwesomeIcons.bars)))));
   }
 }
